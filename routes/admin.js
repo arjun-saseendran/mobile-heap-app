b4a45d0f00b31cce6be260d1eb59bf6012de5365
arjun-saseendran <arjunsaseendran@icloud.com>
2024-07-02 18:21:28 +0530

    view products page created for admin page

---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  const products = [{
+    title: 'iPhone 12 Mini',
+    description: 'Compact, powerful, dual-camera, 5G, OLED display, A14 Bionic, sleek design.',
+    price: '₹26000',
+    image: 'https://rukminim2.flixcart.com/image/850/1000/ko0d6kw0/mobile/d/h/5/iphone-12-mini-mjqh3hn-a-apple-original-imag2k2xuuyfyusd.jpeg?q=20&crop=false'
+  }]
+
+
+
+  res.render('admin/view-products',{products, admin:true});
+});
+
+module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 
 
 
-  res.render('index', { products, admin:true});
+  res.render('index', { products, admin:false});
 });
 
 module.exports = router;
